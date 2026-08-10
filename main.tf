28916d1c11ec34a16c2990f4ba80372dd6fa61e0
cesare-showclix <cesare.ciaccio@showclix.com>
2026-08-10 10:12:04 -0400

    DEVOPS-7347 removed call to deprecated data.template_file

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 ## The following file is responsible for generating and outputing the user-data which can be used to register an instance
 ## with wazuh. It should be concatenated with whatever other userdata scripts we have
 locals {
-  userdata_snippet = templatefile("${path.module}/bash.tpl", {
-    wazuh_manager_ip       = var.wazuh_manager_ip
-    wazuh_manager_password = var.wazuh_manager_password
-    wazuh_agent_groups     = var.wazuh_agent_groups
-  })
+  userdata_snippet = templatefile("${path.module}/bash.tpl", {
+    wazuh_manager_ip       = var.wazuh_manager_ip
+    wazuh_manager_password = var.wazuh_manager_password
+    wazuh_agent_groups     = var.wazuh_agent_groups
+  })
 }
 
 output "userdata_snippet" {
-  description = "Userdata snippet to be added to EC2 instance during bootstrap"
-  value       = local.userdata_snippet
+  description = "Userdata snippet to be added to EC2 instance during bootstrap"
+  value       = local.userdata_snippet
 }
